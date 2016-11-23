6e8ba4c4e20788126da021f8d9051e5301d90d1c
harichandansingh <singh.harichandan@gmail.com>
2016-11-23 17:20:38 -0500

    Working copy with section headers

--- NYT-TopStories/NYT-TopStories/Story.swift
+++ NYT-TopStories/NYT-TopStories/Story.swift
@@ -8,21 +8,31 @@
 
 import Foundation
 
-class Story {
+class Story: NSObject {
     //MARK: - Properties
     let title: String
     let byline: String
     let date: String
     let abstract: String
+    let des_facet: [String]
+    let org_facet: [String]
+    let per_facet: [String]
+    let geo_facet: [String]
     let urlString: String
+    let section: String
     
     //MARK: - Initializers
-    init(title: String, byline: String, date: String, abstract: String, urlString: String) {
+    init(title: String, byline: String, date: String, abstract: String, des_facet: [String], org_facet: [String], per_facet: [String], geo_facet: [String], urlString: String, section: String) {
         self.title = title
         self.byline = byline
         self.date = date
         self.abstract = abstract
+        self.des_facet = des_facet
+        self.org_facet = org_facet
+        self.per_facet = per_facet
+        self.geo_facet = geo_facet
         self.urlString = urlString
+        self.section = section
     }
     
     //MARK: - Methods
@@ -53,16 +63,31 @@ class Story {
                         print("Error finding the abstract key.")
                         continue
                     }
+                    guard let des_facet = element["des_facet"] as? [String] else {
+                        continue
+                    }
+                    guard let org_facet = element["org_facet"] as? [String] else {
+                        continue
+                    }
+                    guard let per_facet = element["per_facet"] as? [String] else {
+                        continue
+                    }
+                    guard let geo_facet = element["geo_facet"] as? [String] else {
+                        continue
+                    }
                     guard let urlString = element["url"] as? String else {
                         print("Error finding the url key.")
                         continue
                     }
-                    let story: Story = Story(title: title, byline: byline, date: date, abstract: abstract, urlString: urlString)
+                    guard let section = element["section"] as? String else {
+                        print("Error finding section key")
+                        continue
+                    }
+                    let story: Story = Story(title: title, byline: byline, date: date, abstract: abstract, des_facet: des_facet, org_facet: org_facet, per_facet: per_facet, geo_facet: geo_facet, urlString: urlString, section: section)
                     allStories.append(story)
                 }
                 return allStories
             }
-            
         }
         catch {
             print("Error encountered when parsing data: \(data)")
