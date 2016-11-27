1f499cdbb7bf50c482330b68cc22704882f31d6c
Kadell <Kadell@MacBook.home>
2016-11-27 14:28:13 -0500

    Changes

--- AC3.2-NYTTopStories/AC3.2-NYTTopStories/Article.swift
+++ AC3.2-NYTTopStories/AC3.2-NYTTopStories/Article.swift
@@ -9,28 +9,36 @@
 import Foundation
 
 enum NewsParseErrorData: Error {
-    case results, title, authors, summary, publishedDate, url
+    case results, title, authors, summary, publishedDate, url, facets
 }
 
 
-class News {
+class Article: NSObject {
     
     let title: String
     let authors: String
     let summary: String
     let published: String
     let url: String
+    let des_facet: [String]
+    let org_facet: [String]
+    let per_facet: [String]
+    let geo_facet: [String]
     
-    init(title: String, authors: String, summary: String, published: String, url: String) {
+    init(title: String, authors: String, summary: String, published: String, url: String, des_facet: [String], org_facet: [String], per_facet: [String], geo_facet: [String] ) {
         self.title = title
         self.authors = authors
         self.summary = summary
         self.published = published
         self.url = url
+        self.des_facet = des_facet
+        self.org_facet = org_facet
+        self.per_facet = per_facet
+        self.geo_facet = geo_facet
     }
     
-    static func newsArticles(from data: Data) -> [News]? {
-        var newsToReturn: [News]? = []
+    static func newsArticles(from data: Data) -> [Article]? {
+        var newsToReturn: [Article]? = []
         
         do{
             let json = try JSONSerialization.jsonObject(with: data, options: [])
@@ -44,9 +52,12 @@ class News {
                 guard let summary = newsDict["abstract"] as? String else {throw NewsParseErrorData.summary}
                 guard let date = newsDict["published_date"] as? String else {throw NewsParseErrorData.publishedDate}
                 guard let url = newsDict["url"] as? String else {throw NewsParseErrorData.url }
+                guard let des = newsDict["des_facet"] as? [String] else {throw NewsParseErrorData.facets}
+                guard let org = newsDict["org_facet"] as? [String] else {throw NewsParseErrorData.facets}
+                guard let per = newsDict["per_facet"] as? [String] else {throw NewsParseErrorData.facets}
+                guard let geo = newsDict["geo_facet"] as? [String] else {throw NewsParseErrorData.facets}
                 
-                
-                let allNews = News(title: title, authors: authors, summary: summary, published: date, url: url )
+                let allNews = Article(title: title, authors: authors, summary: summary, published: date, url: url, des_facet: des, org_facet: org, per_facet: per, geo_facet: geo )
                 newsToReturn?.append(allNews)
             }
         }
@@ -67,6 +78,9 @@ class News {
         }
         catch NewsParseErrorData.url {
             print("Error parsing the URL")
+        }
+        catch NewsParseErrorData.facets {
+            print("Error parsing one of the Facets")
         }
         catch {
             print("Unknown Error")
--- AC3.2-NYTTopStories/AC3.2-NYTTopStories/NYTTopStoriesTableViewController.swift
+++ AC3.2-NYTTopStories/AC3.2-NYTTopStories/NYTTopStoriesTableViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class NYTTopStoriesTableViewController: UITableViewController {
 
-    var allData: [News] = []
+class NYTTopStoriesTableViewController: UITableViewController, UITextFieldDelegate {
+    var allArticles = [Article]()
+    var articles = [Article]()
+    var allData: [Article] = []
     let endPoint = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=12cad604bf9f4e179e59464f10aeb6d2"
     
     
@@ -18,20 +20,26 @@ class NYTTopStoriesTableViewController: UITableViewController {
         super.viewDidLoad()
         
         loadData()
+        navigationItem.title = "Home"
+        
+        self.tableView.estimatedRowHeight = 200
+        self.tableView.rowHeight = UITableViewAutomaticDimension
     }
-
+    
     func loadData() {
         
         APIRequestManager.manager.getData(endPoint: endPoint)
         {( data: Data?) in
-                if let validData = data,
-                    let validNews = News.newsArticles(from: validData) {
-                    self.allData = validNews
-                    
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
+            if let validData = data,
+                let validNews = Article.newsArticles(from: validData) {
+                self.allData = validNews
+                
+                self.articles = self.allArticles
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
                 }
+            }
         }
         
     }
@@ -42,19 +50,19 @@ class NYTTopStoriesTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return allData.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "NewsIdentifier", for: indexPath) as? NewsTableViewCell
@@ -64,24 +72,45 @@ class NYTTopStoriesTableViewController: UITableViewController {
         cell?.authorLabel.text = path.authors + " \n " + path.published
         cell?.summaryLabel.text = path.summary
         
-
+        
         return cell!
     }
     
-
+    
     open override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let url = URL(string: allData[indexPath.row].url)
         UIApplication.shared.open(url!)
         
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func applyPredicate(search: String) {
+        let predicate = NSPredicate(format:"ANY per_facet contains[c] %@", search)
+        self.articles = self.allArticles.filter { predicate.evaluate(with: $0) }
+        self.tableView.reloadData()
+        
+        
     }
-    */
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text {
+            if text.characters.count > 0 {
+                applyPredicate(search: text)
+            }
+            else {
+                self.articles = self.allArticles
+                self.tableView.reloadData()
+            }
+        }
+        return true
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
